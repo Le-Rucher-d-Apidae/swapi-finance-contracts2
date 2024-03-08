364be6696fa0311e4892654e51c269ee6e46dded
BenoistP <75934369+BenoistP@users.noreply.github.com>
2024-03-08 10:56:23 +0100

    Update AptErc20.t.sol

--- test/AptErc20.t.sol
+++ test/AptErc20.t.sol
@@ -7,25 +7,8 @@ import { stdStorage, StdStorage, Test } from "forge-std/src/Test.sol";
 import { Utils } from "./utils/Utils.sol";
 
 import { AptErc20 } from "../src/contracts/AptErc20.sol";
-// import { ERC20InsufficientBalance, ERC20InvalidReceiver } from "@openzeppelin/contracts@5.0.2/interfaces/draft-IERC6093.sol";
 import { IERC20Errors } from "@openzeppelin/contracts@5.0.2/interfaces/draft-IERC6093.sol";
-
-
-interface IERC20 {
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    function name() external view returns (string memory);
-    function symbol() external view returns (string memory);
-    function decimals() external view returns (uint8);
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address owner) external view returns (uint256);
-    function allowance(address owner, address spender) external view returns (uint256);
-
-    function approve(address spender, uint256 value) external returns (bool);
-    function transfer(address to, uint256 value) external returns (bool);
-    function transferFrom(address from, address to, uint256 value) external returns (bool);
-}
+import { IERC20 } from "@openzeppelin/contracts@5.0.2/token/ERC20/IERC20.sol";
 
 contract UsersSetup is Test {
     bool verbose = true;
